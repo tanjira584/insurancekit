7a98e3530c23d68fee39bbc7e54f17f36881a717
Tanjir Ahmed <tanjira584@gmail.com>
2022-02-13 12:44:05 +0600

    header section added

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('toggle-btn').addEventListener('click',function(){
+    var mainMenu = document.getElementById('main-menu');
+    mainMenu.classList.toggle('mobile-menu')
+})
